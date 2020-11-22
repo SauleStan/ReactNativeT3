aad0a8f3e5fdabc863647415fb1e0947243fba82
Saulė Stankevičiūtė <vogellietuva@gmail.com>
2020-11-22 22:06:41 +0200

    AdComponent basic structure complete

--- lib/components/AdComponent.jsx
+++ lib/components/AdComponent.jsx
@@ -3,32 +3,56 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function AdComponent () {
     return(
-        <View style={styles.AdContainer}>
+        <View style={styles.adContainer}>
             {/* Title */}
-            <View>
-                <Text style={styles.AdText}>Title</Text>
+            <View style={styles.adTextArea}>
+                <Text style={styles.adText}>Title</Text>
             </View>
             {/* ad image? */}
             {/* Ad description? */}
-            {/* poster's info? */}
-
+            <View>
+                <Text style={styles.adDescription}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+            </View>
+            {/* poster's info? aka footer*/}
+            <View style={styles.footer}>
+                <Text style={styles.footerText}>Owner: A lil bitchass</Text>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    AdContainer: {
+    adContainer: {
         width: "90%",
-        height: 40,
-        borderWidth: 1,
+        // height: 40,
+        borderWidth: 2,
         margin: 10,
+        backgroundColor: 'yellow',
+        borderBottomRightRadius: 30,
+        borderBottomLeftRadius: 30,
+        borderTopRightRadius: 30,
     },
-    AdText: {
+    adText: {
         fontSize: 20,
         fontWeight: "bold",
         color: "black",
-        // textAlign: 'left',
+        textAlign: 'center',
         padding: 5,
+    },
+    adTextArea: {
+        borderBottomWidth: 1,
+    },
+    adDescription: {
+        padding: 5,
+    },
+    footer: {
+
+    },
+    footerText: {
+        // backgroundColor: 'pink',
+        marginLeft: 20,
+        width: "90%"
     }
+    
 
 })
